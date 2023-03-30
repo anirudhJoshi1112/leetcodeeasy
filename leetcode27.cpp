64df604306c4f55aee1dd006c04b46fcf43f56f5
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2023-03-30 23:14:09 +0530

    Remove Element

---
+++ leetcode27.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+       int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[j++]=nums[i];
+            }
+        }
+        return j;        
+    }
+};
+
+
+/*output->
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]/*
