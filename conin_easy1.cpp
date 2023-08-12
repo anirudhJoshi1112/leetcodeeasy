31c047d42bff8717b2f4ae6d2b9679118cd47a0f
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2023-08-12 10:04:01 +0530

    Area of rectangle

---
+++ conin_easy1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    int area,length,width;
+    cin>>length;
+    cin>>width;
+    cout<<length*width<<endl;
+    return 0;
+}
