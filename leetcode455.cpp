02e68d51f86a8840a22d0d4c68b0a8a2df87b133
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2024-01-01 18:57:26 +0530

    Assign Cookie

---
+++ leetcode455.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int j=0;
+        for(int i=0;j<g.size() && i<s.size();i++){
+            if(g[j]<=s[i])
+            j++;
+        }
+        return j;
+    }
+};
