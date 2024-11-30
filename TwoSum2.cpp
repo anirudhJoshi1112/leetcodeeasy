7f5f6888a0c097e69d69f9e8379bf9f816cab335
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2024-11-30 21:54:50 +0530

    Two Sum

---
+++ TwoSum2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            int compli=target-nums[i];
+
+            if(mp.find(compli)!=mp.end()){
+                return {mp[compli],i};
+            }
+            mp[nums[i]]=i;
+        }
+        return {};
+    }
+};
